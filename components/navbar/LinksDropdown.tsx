c0cd3de254aa5dfdc8fd1ec91bb36abcd20eff73
Suhaib Khan <suhaib0900@gmail.com>
2024-10-25 21:49:38 +0100

    Profile Section Done

--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -3,14 +3,15 @@ import {
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-  // DropdownMenuSeparator,
+  DropdownMenuSeparator,
 } from '@/components/ui/dropdown-menu';
 import { LuAlignLeft } from 'react-icons/lu';
 import Link from 'next/link';
 import { Button } from '../ui/button';
 import UserIcon from './UserIcon';
 import { links } from '@/utils/links';
-// import SignOutLink from './SignOutLink';
+import SignOutLink from './SignOutLink';
+import { SignedOut, SignedIn, SignInButton, SignUpButton } from '@clerk/nextjs';
 
 function LinksDropdown() {
   return (
@@ -21,16 +22,35 @@ function LinksDropdown() {
           <UserIcon />
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-50" align="start" sideOffset={10}>
-        {links.map((link) => {
-          return (
-            <DropdownMenuItem key={link.href}>
-              <Link href={link.href} className="capitalize w-full">
-                {link.label}
-              </Link>
-            </DropdownMenuItem>
-          );
-        })}
+      <DropdownMenuContent className="w-52" align="start" sideOffset={10}>
+        <SignedOut>
+          <DropdownMenuItem>
+            <SignInButton mode="modal" >
+              <button className="w-full text-left">Login</button>
+            </SignInButton>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <SignUpButton mode="modal" >
+              <button className="w-full text-left">Register</button>
+            </SignUpButton>
+          </DropdownMenuItem>
+        </SignedOut>
+        <SignedIn>
+          {links.map((link) => {
+            return (
+              <DropdownMenuItem key={link.href}>
+                <Link href={link.href} className="capitalize w-full">
+                  {link.label}
+                </Link>
+              </DropdownMenuItem>
+            );
+          })}
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <SignOutLink />
+          </DropdownMenuItem>
+        </SignedIn>
       </DropdownMenuContent>
     </DropdownMenu>
   );
--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,6 +1,15 @@
 import { LuUser2 } from 'react-icons/lu';
-
-function UserIcon() {
+import { fetchProfileImage } from '@/utils/actions';
+async function UserIcon() {
+  const ProfileImage = await fetchProfileImage();
+  if (ProfileImage)
+    return (
+      <img
+        src={ProfileImage}
+        alt="profile"
+        className="w-6 h-6 bg-primary rounded-full text-white"
+      />
+    );
   return <LuUser2 className="w-6 h-6 bg-primary rounded-full text-white" />;
 }
 export default UserIcon;
